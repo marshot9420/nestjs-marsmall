07dae3033297851befbfc695ffa25525fe8dd313
marshot <124557537+marshot9420@users.noreply.github.com>
2023-11-23 01:26:34 +0900

    [Deps] TypeORM 추가 & 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { APP } from '@/constants';
+import { dbConfig } from '@/configs';
+import { APP, CONFIG } from '@/constants';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV || APP.NODE_ENV.DEVELOPMENT}`,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get(CONFIG.DB),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
---
+++ src/configs/db.config.ts
@@ -0,0 +1,24 @@
+import { registerAs } from '@nestjs/config';
+
+import { getMetadataArgsStorage } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+import { APP, CONFIG } from '@/constants';
+
+export const dbConfig = registerAs(CONFIG.DB, () => {
+  const isDevMode = process.env.NODE_ENV === APP.NODE_ENV.DEVELOPMENT;
+
+  return {
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mariadb',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
+    charset: 'utf8mb4_unicode_ci',
+    synchronize: isDevMode,
+    logging: isDevMode,
+  };
+});
