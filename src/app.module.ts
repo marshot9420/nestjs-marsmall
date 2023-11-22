9f2476b79552daeeaf1ec7d0c17bbdc8df0db449
marshot <124557537+marshot9420@users.noreply.github.com>
2023-11-23 00:12:58 +0900

    [Chore] @nestjs/config + .env 파일 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { APP } from '@/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || APP.NODE_ENV.DEVELOPMENT}`,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Application {
+  private logger = new Logger(Application.name);
+  private HOST: string;
+  private PORT: string;
+
+  constructor(private app: INestApplication) {
+    this.app = app;
+    this.HOST = process.env.HOST;
+    this.PORT = process.env.PORT;
+  }
+
+  async bootstrap() {
+    this.app = await NestFactory.create(AppModule);
+    await this.app.listen(this.PORT);
+  }
+
+  startLog() {
+    this.logger.log(`✅ Server running on ${this.HOST}:${this.PORT}🚀`);
+  }
 }
-bootstrap();
+
+async function init() {
+  const server = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const app = new Application(server);
+  await app.bootstrap();
+  app.startLog();
+}
+
+init();
